a53596648b03d45b28272459dbd393b6ccb3c77a
Mouss <diarra940.moustapha@gmail.com>
2023-02-02 09:08:39 +0100

    fix. Everything ok

--- functions/src/controllers/SteamUserResumeController.ts
+++ functions/src/controllers/SteamUserResumeController.ts
@@ -2,9 +2,6 @@ import steamUserResumeService from "../services/SteamUserResumeService";
 import {Request, Response } from 'express';
 
 const steamUserResumeController = {
-    getAllSteamUserResumes: async (req: Request, res: Response) => {
-        return steamUserResumeService.getAllSteamUserResumes(res);
-    },
     getSteamUserResumeBySteamId:  (req: Request, res: Response) => {
         return steamUserResumeService.getSteamUserResumeBySteamId(req, res);
     }
--- functions/src/routes/SteamUserResumeRoute.ts
+++ functions/src/routes/SteamUserResumeRoute.ts
@@ -3,7 +3,6 @@ import steamUserResumeController from "../controllers/SteamUserResumeController"
 
 const router = Router();
 
-router.get('/', steamUserResumeController.getAllSteamUserResumes);
 router.get('/steamUserId/:steamUserId', steamUserResumeController.getSteamUserResumeBySteamId);
 
 export default router;
